723d7c35539f49c5af4fc164b2a0d86dd041c186
Ashutosh kumar <108188754+singhashutosh200@users.noreply.github.com>
2022-12-13 00:04:55 +0530

    Update facebooklogin.py

--- facebooklogin.py
+++ facebooklogin.py
@@ -8,8 +8,8 @@ driver.get("https://www.facebook.com")
 print(driver.current_url)
 print(driver.title)
 time.sleep(5)
-driver.find_element(By.XPATH, "//input[@placeholder='Email address or phone number']").send_keys("XXXXXX")
-driver.find_element(By.XPATH, "//input[@type='password']").send_keys("XXXXXX")
+driver.find_element(By.XPATH, "//input[@placeholder='Email address or phone number']").send_keys("XXXXXX")  #please enter the valid email address or phone number.
+driver.find_element(By.XPATH, "//input[@type='password']").send_keys("XXXXXX")  #please enter the valid password
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
 print(driver.title)
 print(driver.current_url)
